bedb49b13d218c257f66b5bafc7bbff418bb927d
Renee Wallis <2560885w@student.gla.ac.uk>
2024-01-07 20:28:33 +0000

    fixed getDataPoint and getRatio and main in client.py, added unit tests for getDataPoint and getRatio in client_test.py

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,10 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      ask_price = quote['top_ask']['price']
+      bid = quote['top_bid']['price']
+      self.assertEqual(getDataPoint(quote), (quote['stock'], bid, ask_price, (bid+ask_price)/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +19,40 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+      ask_price = quote['top_ask']['price']
+      bid = quote['top_bid']['price']
+      self.assertEqual(getDataPoint(quote), (quote['stock'], bid, ask_price, (bid+ask_price)/2 ))
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio(self):
+    prices = {}
+    quotes = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453',
+       'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453',
+       'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    for quote in quotes:
+      stock, bid_price, ask_price, price = getDataPoint(quote)
+      prices[stock] = price
+    """ ------------ Add the assertion below ------------ """
+    self.assertEqual(getRatio(prices['ABC'], prices['DEF']), prices['ABC']/prices['DEF'])
+
+  def test_getRatio_stockBPriceIsZero(self):
+    prices = {'DEF': 0, 'ABC': 118.7}
+    """ ------------ Add the assertion below ------------ """
+    self.assertEqual(getRatio(prices['ABC'], prices['DEF']), None)
+
+  def test_getRatio_stockAPriceIsZero(self):
+    prices = {'ABC': 0, 'DEF' : 900.32}
+    """ ------------ Add the assertion below ------------ """
+    self.assertEqual(getRatio(prices['ABC'], prices['DEF']), 0)
+
+  def test_getRatio_stockAPriceLarger(self):
+    prices = {'ABC' : 100, 'DEF':50}
+    self.assertEqual(getRatio(prices['ABC'], prices['DEF']), prices['ABC']/prices['DEF'])
+
 
 
 
